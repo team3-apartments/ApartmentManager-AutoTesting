869a0513510147bf5eb1f2bcd0238a5297e8fdeb
Husnan Sultan <husnansultan@academytrainee.com>
2019-02-21 16:49:01 +0000

    a
    a

--- IntegrationTests/src/test/java/com/qa/IntegrationTests/HomepageTests.java
+++ IntegrationTests/src/test/java/com/qa/IntegrationTests/HomepageTests.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;	
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
